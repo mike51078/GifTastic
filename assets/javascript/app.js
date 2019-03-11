635c0fc6a8efa2f23967e07e3ece3b30f735c1bb
elcid00 <elcid00@aol.com>
2019-03-10 22:56:41 -0400

    still working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,54 +5,39 @@
       function displayCharacterInfo() {
 
         var character = $(this).attr("data-name");
-        var queryURL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + character + "&api_key=wABYUPbOL923Ngb9coOwmarOJ0o6rh4m&limit=10";
 
         $.ajax({
-          url: queryURL,
-          method: "GET"
+            url: queryURL,
+             method: "GET"
         }).then(function(response) {
+            console.log(response);
 
-          var characterDiv = $("<div class='character'>");
+            var results = response.data;
+            
+            var characterDiv = $("<div class='character'>");
 
-          // Storing the rating data
-          var rating = response.Rated;
+            // Storing the rating data
+            var rating = response.Rated;
 
-          // Creating an element to have the rating displayed
-          var pOne = $("<p>").text("Rating: " + rating);
+            // Creating an element to have the rating displayed
+            var pOne = $("<p>").text("Rating: " + rating);
 
-          // Displaying the rating
-          movieDiv.append(pOne);
+            // Displaying the rating
+            characterDiv.append(pOne);
 
-          // Storing the release year
-          var released = response.Released;
+                                            //   // Retrieving the URL for the image
+                                            //   var imgURL = response.Poster;
 
-          // Creating an element to hold the release year
-          var pTwo = $("<p>").text("Released: " + released);
+                                            //   // Creating an element to hold the image
+                                            //   var image = $("<img>").attr("src", imgURL);
 
-          // Displaying the release year
-          movieDiv.append(pTwo);
+                                            //   // Appending the image
+                                            //   characterDiv.append(image);
 
-          // Storing the plot
-          var plot = response.Plot;
-
-          // Creating an element to hold the plot
-          var pThree = $("<p>").text("Plot: " + plot);
-
-          // Appending the plot
-          movieDiv.append(pThree);
-
-          // Retrieving the URL for the image
-          var imgURL = response.Poster;
-
-          // Creating an element to hold the image
-          var image = $("<img>").attr("src", imgURL);
-
-          // Appending the image
-          movieDiv.append(image);
-
-          // Putting the entire movie above the previous movies
-          $("#buttons-view").prepend(movieDiv);
-        });
+            // Putting the entire movie above the previous movies
+            $("#buttons-view").prepend(characterDiv);
+                });
 
       }
 
@@ -60,21 +45,21 @@
 
         $("#buttons-view").empty();
 
-        for (var i = 0; i < movies.length; i++) {
+        for (var i = 0; i < characters.length; i++) {
 
           var a = $("<button>");
 
           a.addClass("character-btn");
 
-          a.attr("data-name", character[i]);
+          a.attr("data-name", characters[i]);
 
-          a.text(character[i]);
+          a.text(characters[i]);
 
           $("#buttons-view").append(a);
         }
       }
 
-      $("#add-character").on("click", function(event) {
+      $('#add-character').on("click", function(event) {
         event.preventDefault();
 
         var character = $("#character-input").val().trim();
